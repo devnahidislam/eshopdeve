8b52b73679cf42dc160972c6c5ecf3513ff05c30
mdnahidislam <mdnahidislam30@gmail.com>
2022-01-21 16:28:33 +0600

    Herokuapp test

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth', 'isAdmin'])->group(function () {
+    Route::get('/dashboard', function(){
+        return "This is admin dashboard.";
+    });
+});
